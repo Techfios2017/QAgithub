c068a04e3fd1b18dfda724545f5f1581ab908500
Tanvir <sk_nawaz@yahoo.com>
2020-02-11 15:29:36 -0600

    commit

---
+++ GitHubTrial/src/main/java/Github/GitHubTrial/Encap.java
@@ -0,0 +1,19 @@
+package class6Review;
+
+public class Encap {
+
+	public static void main(String[] args) {
+		System.out.println("main");
+		//System.out.println(GetSet.abc() +45);
+	GetSet object=new GetSet();
+	System.out.println(GetSet.a);
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+}
