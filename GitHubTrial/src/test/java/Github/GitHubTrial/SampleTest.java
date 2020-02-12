03b09356831bd807d706e2c641d6f9277bdf3376
swapna melugiri <melugiriswapna@gmail.com>
2020-02-11 22:25:43 -0600

    this is my sample test case.

---
+++ GitHubTrial/src/test/java/Github/GitHubTrial/SampleTest.java
@@ -0,0 +1,14 @@
+package Github.GitHubTrial;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void verifyTwoNumbers() {
+		int a = 2;
+		int b = 5;
+		Assert.assertTrue(a+b == 7);
+	}
+
+}
