a80ff1a7c9ed9953da95d1ba8ce6a97b2f816cf5
Tanvir <sk_nawaz@yahoo.com>
2020-02-20 23:01:19 -0600

    test

---
+++ GitHubTrial/src/test/java/Github/GitHubTrial/GetSet.java
@@ -0,0 +1,15 @@
+package Github.GitHubTrial;
+
+public class GetSet {
+
+	static int a = 92;
+
+	public int GetA() {
+		return this.a;
+	}
+
+	public void SetA(int newValue) {
+		this.a = newValue;
+	}
+
+}
